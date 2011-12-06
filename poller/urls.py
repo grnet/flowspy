a387277a1684ff73d31daf7defbee0d516967b8b
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2011-12-06 11:09:28 +0200

    Modified long-polling urls

--- poller/urls.py
+++ poller/urls.py
@@ -3,9 +3,9 @@ from django.conf import settings
 
 urlpatterns = patterns('flowspy.poller.views',
                        ('^$', 'main'),
-                       url('^a/message/existing$', 'message_existing', name='fetch-existing'),
-                       url('^a/message/new$', 'message_new',name='fetch-new'),
-                       url('^a/message/updates$', 'message_updates', name='fetch-updates'))
+                       url('^message/existing$', 'message_existing', name='fetch-existing'),
+                       url('^message/new$', 'message_new',name='fetch-new'),
+                       url('^message/updates$', 'message_updates', name='fetch-updates'))
 
 urlpatterns += patterns('',
     (r'^static/(?P<path>.*)', 'django.views.static.serve',\

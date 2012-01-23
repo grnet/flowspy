e0a357923b3768d086d771c6ca779dd5bb16e705
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2012-01-23 15:33:15 +0200

    Removed peer lookup and user profile from shibauth backend

--- djangobackends/shibauthBackend.py
+++ djangobackends/shibauthBackend.py
@@ -3,8 +3,6 @@
 
 from django.contrib.auth.models import User, UserManager, Permission, Group
 from django.conf import settings
-from flowspy.peers.models import *
-from flowspy.accounts.models import * 
 
 class shibauthBackend:
     def authenticate(self, **kwargs):
@@ -12,46 +10,32 @@ class shibauthBackend:
         firstname = kwargs.get('firstname')
         lastname = kwargs.get('lastname')
         mail = kwargs.get('mail')
-        affiliation = kwargs.get('affiliation')
-        organization = kwargs.get('organization')
         try:
-            user = self._auth_user(username, firstname, lastname, mail, affiliation, organization)
+            user = self._auth_user(username, firstname, lastname, mail)
         except:
             return None
         if not user:
             return None
         return user
 
-    def _auth_user(self, username, firstname, lastname, mail, affiliation, organization):
+    def _auth_user(self, username, firstname, lastname, mail):
 
         try:
             user = User.objects.get(username__exact=username)
             user.email = mail
             user.first_name = firstname
             user.last_name = lastname
-#            if organization == settings.SHIB_ADMIN_DOMAIN:
-#                user.is_staff = True
-#                user.is_superuser = True
-#            user.is_active = True
             user.save()
         # The user did not exist. Create one with no privileges
-        except:
-            
+        except: 
             user = User.objects.create_user(username, mail, None)
             user.first_name = firstname
             user.last_name = lastname
             user.is_staff = False
             user.is_superuser = False
-#            if organization == settings.SHIB_ADMIN_DOMAIN:
-#                user.is_staff = True
-#                user.is_superuser = True
             user.is_active = False
             user.save()
-        try:
-            peer = Peer.objects.get(domain_name=organization)
-            up = UserProfile.objects.get_or_create(user=user,peer=peer)
-        except:
-            return False
+
         return user
 
     def get_user(self, user_id):

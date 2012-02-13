ddffea2d5db44a0b518673e581fc9d8058124f1c
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2012-02-13 18:19:32 +0200

    Added FQDN resolving in mail notification templates to denote the host that an action originated

---
+++ flowspec/templatetags/tofqdn.py
@@ -0,0 +1,18 @@
+from django import template
+from django.utils.safestring import mark_safe
+from django.utils.encoding import force_unicode
+import socket
+
+register = template.Library()
+
+@register.filter
+def tofqdn(value):
+    try:
+        fqdn = socket.getfqdn(value)
+        if fqdn == value:
+            return False
+        else:
+            return fqdn
+    except socket.gaierror:
+        return False
+        

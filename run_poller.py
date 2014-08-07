88e06603ab4b69c145036247975a558f5b411c85
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2014-08-07 13:48:11 +0300

    Remove obsolete file

--- run_poller.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from gevent.wsgi import WSGIServer
-from poller.application import application
-server="localhost"
-port=8081
-print 'Serving on port %s...' % port
-WSGIServer((server,port ), application).serve_forever()

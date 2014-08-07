adc07310c041d675d2e6712d70202ea419e67c30
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2014-08-07 13:44:47 +0300

    Cleanup poller urls

--- poller/urls.py
+++ poller/urls.py
@@ -18,15 +18,10 @@
 #SOFTWARE.
 #
 from django.conf.urls.defaults import *
-from django.conf import settings
 
 urlpatterns = patterns('poller.views',
                        ('^$', 'main'),
                        url('^message/existing$', 'message_existing', name='fetch-existing'),
-                       url('^message/new$', 'message_new',name='fetch-new'),
                        url('^message/updates$', 'message_updates', name='fetch-updates'))
 
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)', 'django.views.static.serve',\
-        {'document_root':  settings.STATIC_URL}),
-)
+

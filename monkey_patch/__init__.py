da719daa431dc2b87b10b5709d390c8db861898d
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2014-08-28 11:50:52 +0300

    Remove obsolete app

--- monkey_patch/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-# This patch was retrieved from: https://github.com/GoodCloud/django-longer-username
-def MAX_USERNAME_LENGTH():
-    if hasattr(settings,"MAX_USERNAME_LENGTH"):
-        return settings.MAX_USERNAME_LENGTH
-    else:
-        return 255

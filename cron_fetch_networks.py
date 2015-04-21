8e1cf2ec014456914d1793032401086d3592ff35
Stauros Kroustouris <staurosk@noc.grnet.gr>
2015-04-21 19:49:59 +0300

    update cron_fetch_networks

--- cron_fetch_networks.py
+++ cron_fetch_networks.py
@@ -1,17 +1,15 @@
-from django.core.management import setup_environ  
-import settings
-setup_environ(settings)
-from django.core.mail import send_mail
-from flowspy.peers.models import *
-
+#!/usr/bin/env python
+import os
+import sys
 
 def populate_networks():
     peers = Peer.objects.all()
     for peer in peers:
+        print peer
         peer.fill_networks()
-#    if peer_networks > 0:
-#        send_mail('Peer networks added , 'noreply@grnet.gr', ['leopoul@noc.grnet.gr'])
-
 
 if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flowspy.settings")
+    from django.core.mail import send_mail
+    from peers.models import *
     populate_networks()

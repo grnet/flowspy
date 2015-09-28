7ed6dcd07bd58cc0b02a173965c3e877f71f0364
Stauros Kroustouris <staurosk@noc.grnet.gr>
2015-09-28 15:56:24 +0300

    fix render

--- flowspec/views.py
+++ flowspec/views.py
@@ -638,6 +638,7 @@ def add_port(request):
             )
         else:
             return render(
+                request,
                 'add_port.html',
                 {
                     'form': form,
